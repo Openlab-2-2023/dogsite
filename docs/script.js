3e12a4e536c4ecca5d28ad045759a66229d86e24
UserB3nj4m1n <benjaminko784@gmail.com>
2023-11-28 09:48:50 +0100

    Update

--- docs/script.js
+++ docs/script.js
@@ -7,3 +7,36 @@ function myFunction() {
       x.className = "nav";
     }
   }
+
+  const text = "Hello world!";
+  let index = 0;
+  const typingContainer = document.getElementById('typing');
+  
+  // Set up the Intersection Observer
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Start typing when the element is in view
+        typingContainer.classList.remove('hidden');
+        startTyping();
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.5 });
+  
+  // Observe the typing container
+  observer.observe(typingContainer);
+  
+  function startTyping() {
+    const typingInterval = setInterval(type, 100);
+  
+    function type() {
+      typingContainer.textContent = text.slice(0, index);
+      index++;
+  
+      if (index > text.length) {
+        clearInterval(typingInterval);
+      }
+    }
+  }
+  
